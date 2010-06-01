b63449c08b2cc01cff0baf2d60c567e4ce405fae
sugyan <sugi1982@gmail.com>
2010-06-01 21:08:19 +0900

    emacs設定追加

--- .emacs.el
+++ .emacs.el
@@ -3,11 +3,13 @@
 (add-to-list 'load-path "~/.emacs.d/auto-install")
 
 ;; init-loader
+;; http://coderepos.org/share/browser/lang/elisp/init-loader/init-loader.el
 ;; デフォルトで"~/.emacs.d/inits"以下のファイルをロードする
 (require 'init-loader)
 (init-loader-load)
 
 ;; auto-install
+;; http://www.emacswiki.org/emacs/auto-install.el
 (require 'auto-install)
 (setq auto-install-directory "~/.emacs.d/auto-install/")
 (auto-install-update-emacswiki-package-name t)
