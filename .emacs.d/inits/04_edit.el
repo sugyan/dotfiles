0e717f1f72858affffd4c6d33b63d7b6f8d17680
sugyan <sugi1982@gmail.com>
2010-06-01 22:21:45 +0900

    auto-complete設定を追加

--- .emacs.d/inits/04_edit.el
+++ .emacs.d/inits/04_edit.el
@@ -4,3 +4,9 @@
 (setq-default tab-width 4 indent-tabs-mode nil)
 ;; 対応する括弧のハイライト
 (show-paren-mode t)
+;; auto-complete
+(require 'auto-complete)
+(global-auto-complete-mode t)
+(define-key ac-complete-mode-map (kbd "C-n") 'ac-next)
+(define-key ac-complete-mode-map (kbd "C-p") 'ac-previous)
+(define-key ac-complete-mode-map (kbd "C-j") 'ac-complete)
