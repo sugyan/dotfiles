0e70cac0e857d7a037dbc3e3ed092f83da7e1d80
sugyan <sugi1982@gmail.com>
2010-06-01 14:59:46 +0900

    emacs設定

---
+++ .emacs.d/inits/00_global-keys.el
@@ -0,0 +1,4 @@
+(global-set-key "\C-h" 'delete-backward-char)
+(global-set-key "\C-o" 'other-window)
+(setq ns-alternate-modifier 'alt)
+(setq ns-command-modifier 'meta)
---
+++ .emacs.el
@@ -0,0 +1,4 @@
+;; load-path
+(add-to-list 'load-path "~/.emacs.d/elisp")
+(require 'init-loader)
+(init-loader-load)
