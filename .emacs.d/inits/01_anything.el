49be2a98d8533319d2c37474ad8129a92960b7bb
sugyan <sugi1982@gmail.com>
2010-06-01 17:50:40 +0900

    anything設定追加

--- .emacs.d/inits/01_anything.el
+++ .emacs.d/inits/01_anything.el
@@ -1,3 +1,9 @@
 (require 'anything-config)
-(setq anything-source (list anything-c-source-buffers))
+(setq anything-sources (list anything-c-source-buffers
+			     anything-c-source-recentf
+			     anything-c-source-extended-command-history
+			     anything-c-source-emacs-commands))
+(define-key anything-map (kbd "C-j") 'anything-exit-minibuffer) ; "C-j"で決定したときに別frameが開いてしまうのを防ぐ
+(define-key anything-map (kbd "M-v") 'anything-previous-source)
+(define-key anything-map (kbd "C-v") 'anything-next-source)
 (global-set-key (kbd "C-;") 'anything)
