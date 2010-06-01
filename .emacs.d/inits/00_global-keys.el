b09cc8f2f3a834fc1a547b5356610664f4f4fdb6
sugyan <sugi1982@gmail.com>
2010-06-01 15:37:31 +0900

    emacs auto-install.elの設定を追加

--- .emacs.d/inits/00_global-keys.el
+++ .emacs.d/inits/00_global-keys.el
@@ -1,4 +1,6 @@
-(global-set-key "\C-h" 'delete-backward-char)
-(global-set-key "\C-o" 'other-window)
+(global-set-key "\C-h" 'delete-backward-char) ;; 一文字削除
+(global-set-key "\C-o" 'other-window)         ;; 別windowへ移動
+
+;; METAキーとALTキー交換
 (setq ns-alternate-modifier 'alt)
 (setq ns-command-modifier 'meta)
--- .emacs.el
+++ .emacs.el
@@ -1,4 +1,12 @@
 ;; load-path
 (add-to-list 'load-path "~/.emacs.d/elisp")
+
+;; init-loader
+;; デフォルトで"~/.emacs.d/inits"以下のファイルをロードする
 (require 'init-loader)
 (init-loader-load)
+
+;; auto-install
+(require 'auto-install)
+(setq auto-install-directory "~/.emacs.d/auto-install/")
+(auto-install-update-emacswiki-package-name t)
