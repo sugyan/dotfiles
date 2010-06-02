06815e6b0db4c16fce19ecace897dc6b3f64934a
sugyan <sugi1982@gmail.com>
2010-06-02 19:06:56 +0900

    psgiファイルをperl-modeに

--- .emacs.d/inits/05_perl.el
+++ .emacs.d/inits/05_perl.el
@@ -3,3 +3,6 @@
           (lambda ()
             (require 'perl-completion)
             (perl-completion-mode t)))
+(setq auto-mode-alist
+      (append '(("\\.psgi\\'" . perl-mode))
+              auto-mode-alist))
