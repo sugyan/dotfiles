3a4b2482f59988035c8145925287021494bfb736
sugyan <sugi1982@gmail.com>
2010-06-04 11:20:51 +0900

    shellの設定

---
+++ .emacs.d/inits/06_shell.el
@@ -0,0 +1,19 @@
+;; 以下のコマンドでterminfoを生成しておく
+;; tic -o $HOME/.terminfo path/to/Emacs.app/Contents/Resources/etc/e/eterm-color.ti
+(require 'multi-term)
+
+;; Macの場合 utf-8-hfs にすべき？
+(setq locale-coding-system 'utf-8)
+
+;; shell-command
+(global-set-key "\M-s" 'multi-term-next)
+
+;; term に奪われたくないキー
+(add-to-list 'term-unbind-key-list '"C-o")
+
+;; term 内での文字削除、ペーストを有効にする
+(add-hook 'term-mode-hook
+          '(lambda ()
+             (define-key term-raw-map (kbd "C-h") 'term-send-backspace)
+             (define-key term-raw-map (kbd "C-y") 'term-paste)
+             ))
--- .zshenv
+++ .zshenv
@@ -4,3 +4,5 @@ typeset -U path
 path=($HOME/local/bin(N) $path)
 # perlbrew
 path=($HOME/perl5/perlbrew/bin(N) $HOME/perl5/perlbrew/perls/current/bin(N) $path)
+
+export LANG=ja_JP.UTF-8
