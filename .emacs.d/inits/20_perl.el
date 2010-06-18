8e22c19a136dbcbd543c4c4e7e8c4bf1bc04a977
sugyan <sugi1982@gmail.com>
2010-06-18 12:19:04 +0900

    .tファイルでproveを実行できるように http://d.hatena.ne.jp/sugyan/20100618/1276829722

--- .emacs.d/inits/20_perl.el
+++ .emacs.d/inits/20_perl.el
@@ -5,17 +5,46 @@
       cperl-close-paren-offset -4
       cperl-indent-parens-as-block t)
 
-;; perl-completion
-(add-hook 'cperl-mode-hook
-          (lambda ()
-            (require 'perl-completion)
-            (perl-completion-mode t)))
 
 ;; 対象拡張子の追加
 (setq auto-mode-alist
       (append '(("\\.psgi\\'" . perl-mode)
                 ("\\.t\\'" . perl-mode))
               auto-mode-alist))
+
+;; *.t ファイルに対して prove コマンドで compile する
+(defvar root-files '("Makefile.PL"))
+(defun current-directory ()
+  (file-name-directory (expand-file-name (or (buffer-file-name)
+                                             default-directory))))
+(defun perl-get-root-directory ()
+  (loop with i = 0
+        with cur-dir = (current-directory)
+        while (< i 10)
+        do (if (some
+                (lambda (file) (find file (directory-files cur-dir) :test 'string=))
+                root-files)
+               (return cur-dir))
+        (setq cur-dir (expand-file-name (concat cur-dir "../")))
+        (incf i)
+        finally return nil))
+(defun perl-run-test ()
+  (interactive)
+  (let ((compile-command
+         (cond ((string-match "\\.t$" (buffer-file-name))
+                (let ((root-dir (perl-get-root-directory)))
+                  (concat (if root-dir
+                              (concat "cd " root-dir "; "))
+                          "prove --nocolor -l \"" (expand-file-name (buffer-file-name)) "\"")))
+               (t (concat "perl -c " (buffer-file-name))))))
+    (call-interactively 'compile)))
+
+;; hook
+(add-hook 'cperl-mode-hook
+          (lambda ()
+            (define-key cperl-mode-map (kbd "C-c p") 'perl-run-test)
+            (require 'perl-completion) ;; perl-completion
+            (perl-completion-mode t)))
 
 ;; ttはhtml-modeでごまかす
 (add-to-list 'auto-mode-alist '("\\.tt\\'" . html-mode))
