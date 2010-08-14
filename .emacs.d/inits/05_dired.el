7691130ff8a8c75b19b8d2564964ac4905d68211
sugyan <sugi1982@gmail.com>
2010-08-14 23:27:51 +0900

    dired-modeの設定追加

--- .emacs.d/inits/05_dired.el
+++ .emacs.d/inits/05_dired.el
@@ -1,5 +1,6 @@
-(add-hook 'dired-mode-hook
-          (lambda ()
-            (define-key dired-mode-map (kbd "o")   'dired-display-file)
-            (define-key dired-mode-map (kbd "C-o") 'other-window)
-            (define-key dired-mode-map (kbd "C-s") 'dired-isearch-filenames)))
+(defun my-dired-mode-hook ()
+  (define-key dired-mode-map (kbd "o")   'dired-display-file)
+  (define-key dired-mode-map (kbd "r")   'wdired-change-to-wdired-mode)
+  (define-key dired-mode-map (kbd "C-o") 'other-window)
+  (define-key dired-mode-map (kbd "C-s") 'dired-isearch-filenames))
+(add-hook 'dired-mode-hook 'my-dired-mode-hook)
