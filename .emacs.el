6d82e766cde18214bcfbdb766ed2165cc4f01886
sugyan <sugi1982@gmail.com>
2011-02-07 09:19:52 +0900

    http://d.hatena.ne.jp/ymotongpoo/20110206/1296983970

--- .emacs.el
+++ .emacs.el
@@ -16,3 +16,16 @@
 (require 'auto-install)
 (setq auto-install-directory "~/.emacs.d/auto-install/")
 (auto-install-update-emacswiki-package-name t)
+
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(vc-follow-symlinks t))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ )
