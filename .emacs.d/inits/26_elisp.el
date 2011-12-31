1db417ac646dd86aac9aebe134de1ff87222527d
sugyan <sugi1982@gmail.com>
2011-12-31 17:23:36 +0900

    add auto-async-byte-compile

---
+++ .emacs.d/inits/26_elisp.el
@@ -0,0 +1,4 @@
+;;; 26_elisp.el --- 
+;; (auto-install-from-emacswiki "auto-async-byte-compile.el")
+(require 'auto-async-byte-compile)
+(add-hook 'emacs-lisp-mode-hook 'enable-auto-async-byte-compile-mode)
---
+++ .emacs.d/minimum.el
@@ -0,0 +1,4 @@
+;; minimum.el
+(setq ns-alternate-modifier 'alt)
+(setq ns-command-modifier 'meta)
+(define-key global-map (kbd "C-h") 'delete-backward-char)
