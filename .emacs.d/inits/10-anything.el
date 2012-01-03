0cb6812543b934030948d0326d65c8a8ac9398b3
sugyan <sugi1982@gmail.com>
2012-01-03 14:12:20 +0900

    emacs: use anything-command-map

--- .emacs.d/inits/10-anything.el
+++ .emacs.d/inits/10-anything.el
@@ -3,15 +3,13 @@
 (require 'anything-config)
 (setq anything-input-idle-delay 0.1)
 
+;; key
+(define-key global-map (kbd "C-z") 'anything-command-map)
+
+;; replace commands
 (define-key global-map (kbd "C-x b") 'anything-for-files)
 (define-key global-map (kbd "M-y")   'anything-show-kill-ring)
-(define-key global-map (kbd "M-x")
-  (lambda ()
-    (interactive)
-    (anything-other-buffer
-     '(anything-c-source-extended-command-history
-       anything-c-source-emacs-commands)
-     "*anything emacs commands*")))
+(define-key global-map (kbd "M-x")   'anything-M-x)
 
 ;; my anything
 (defun my-anything ()
