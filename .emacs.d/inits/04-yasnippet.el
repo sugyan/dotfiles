e41d27868674a098c64635678239ac38feb25d39
sugyan <sugi1982@gmail.com>
2012-01-04 11:14:37 +0900

    remove dependence on yasnippet

--- .emacs.d/inits/04-yasnippet.el
+++ .emacs.d/inits/04-yasnippet.el
@@ -1,5 +1,5 @@
 ;;; Yasnippet
 ;; (auto-install-from-url "https://raw.github.com/capitaomorte/yasnippet/master/yasnippet.el")
 (require 'yasnippet)
-(setq yas/snippet-dirs '("~/.emacs.d/inits/etc/snippets" "~/.emacs.d/snippets"))
+(setq-default yas/snippet-dirs '("~/.emacs.d/inits/etc/snippets"))
 (yas/global-mode t)
