25f68e8f2dec5957f2093efe944697aa5c1e8e53
sugyan <sugi1982@gmail.com>
2012-01-04 23:22:13 +0900

    emacs: minimum init-loader, remove auto-install-update-emacswiki-package-name

--- .emacs.el
+++ .emacs.el
@@ -1,3 +1,41 @@
+;; load-path
+(add-to-list 'load-path "~/.emacs.d/elisp")
+(add-to-list 'load-path "~/.emacs.d/auto-install")
+
+;; init loader
+(eval-when-compile (require 'cl))
+(defgroup init-loader nil
+  "init loader"
+  :group 'init-loader)
+(defcustom init-loader-directory (expand-file-name "~/.emacs.d/inits")
+  "default directory"
+  :type 'directory
+  :group 'init-loader)
+(defcustom init-loader-default-regexp "\\(?:^[[:digit:]]\\{2\\}-.*\.elc?\\)$"
+  "default filename regexp"
+  :type 'regexp
+  :group 'init-loader)
+(let* ((init-dir init-loader-directory)
+       (load-path (cons init-dir load-path))
+       (files (directory-files init-dir t))
+       (targets (loop for file in (directory-files init-dir t)
+                      when (let ((file (file-name-nondirectory file)))
+                             (and (string-match init-loader-default-regexp file)
+                                  (or (and (string-match "\.el$" file)
+                                           (or (not (locate-library (concat file "c")))))
+                                      (string-match "\.elc$" file))))
+                      collect file into ret
+                      finally return (sort ret 'string<))))
+  (dolist (target targets)
+    (condition-case e
+        (load (file-name-sans-extension target))
+      (error (message "%s" e)))))
+
+;; auto-install
+;; http://www.emacswiki.org/emacs/auto-install.el
+;; (auto-install-from-emacswiki "auto-install.el")
+(require 'auto-install)
+
 ;; benchmark
 (defun my-show-init-time ()
   (interactive)
@@ -5,28 +43,3 @@
            (+ (* (- (nth 1 after-init-time) (nth 1 before-init-time)) 1000)
               (/ (- (nth 2 after-init-time) (nth 2 before-init-time)) 1000))))
 (add-hook 'after-init-hook 'my-show-init-time)
-
-;; load-path
-(add-to-list 'load-path "~/.emacs.d/elisp")
-(add-to-list 'load-path "~/.emacs.d/auto-install")
-
-;; auto-install
-;; http://www.emacswiki.org/emacs/auto-install.el
-;; (auto-install-from-emacswiki "auto-install.el")
-(require 'auto-install)
-(auto-install-update-emacswiki-package-name t)
-
-;;; init-loader
-;; (auto-install-from-url "http://coderepos.org/share/export/39091/lang/elisp/init-loader/init-loader.el")
-(require 'init-loader)
-;; loads hoge.elc (if exists) or hoge.el
-(eval-when-compile (require 'cl))
-(flet ((init-loader--re-load-files (re dir &optional sort)
-  (loop for el in (directory-files dir t)
-        when (and (string-match re (file-name-nondirectory el))
-                  (or (string-match "elc$" el)
-                      (and (string-match "el$" el)
-                           (not (locate-library (concat el "c"))))))
-        collect (file-name-nondirectory el) into ret
-        finally return (if sort (sort ret 'string<) ret))))
-  (init-loader-load))
