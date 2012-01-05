3ef08fac8e3048df6f7beb13b07f6adf00234542
sugyan <sugi1982@gmail.com>
2012-01-05 09:02:27 +0900

    emacs: anything settings

--- .emacs.d/inits/10-anything.el
+++ .emacs.d/inits/10-anything.el
@@ -9,17 +9,16 @@
 
 ;; replace commands
 (define-key global-map (kbd "C-x b") 'anything-for-files)
-(define-key global-map (kbd "C-s")   'anything-occur)
 (define-key global-map (kbd "M-y")   'anything-show-kill-ring)
 (define-key global-map (kbd "M-x")   'anything-M-x)
 (define-key anything-command-map (kbd "r") 'anything-resume)
+(define-key anything-command-map (kbd "o") 'anything-occur)
 
 ;; my anything
 (defun my-anything ()
   (interactive)
   (anything-other-buffer
    '(anything-c-source-buffers-list
-     anything-c-source-recentf
-     anything-c-source-extended-command-history)
+     anything-c-source-recentf)
    "*anything my-anything*"))
 (define-key global-map (kbd "C-;") 'my-anything)
