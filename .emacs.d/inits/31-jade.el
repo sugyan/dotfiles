6641c180cb923fa58980874ecf825c5766a1b62e
sugyan <sugi1982@gmail.com>
2012-01-07 15:16:45 +0900

    emacs: add jade-mode

---
+++ .emacs.d/inits/31-jade.el
@@ -0,0 +1,8 @@
+;;; Jade
+
+;; jade-mode
+;; (auto-install-from-url "https://raw.github.com/brianc/jade-mode/master/sws-mode.el")
+;; (auto-install-from-url "https://raw.github.com/brianc/jade-mode/master/jade-mode.el")
+(autoload 'sws-mode "sws-mode" nil t)
+(autoload 'jade-mode "jade-mode" nil t)
+(add-to-list 'auto-mode-alist '("\\.jade$" . jade-mode))
