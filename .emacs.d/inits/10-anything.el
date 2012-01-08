db734326ea6320fda7118b987a0ac7aaa6dccf65
sugyan <sugi1982@gmail.com>
2012-01-09 02:03:51 +0900

    emacs: dont load anything in init

--- .emacs.d/inits/10-anything.el
+++ .emacs.d/inits/10-anything.el
@@ -1,33 +1,32 @@
 ;;; anything
 ;; (auto-install-batch "anything-minimal")
-(require 'anything-config)
-(setq anything-input-idle-delay 0.1)
 
-(custom-set-variables
- ;; prefix key
- '(anything-command-map-prefix-key "C-z"))
+;; load anything-config
+(define-key global-map (kbd "C-z")
+  (lambda () (interactive)
+    (require 'anything-config)))
 
-;; replace commands
-(define-key global-map (kbd "C-x b") 'anything-for-files)
-(define-key global-map (kbd "M-y")   'anything-show-kill-ring)
-(define-key global-map (kbd "M-x")   'anything-M-x)
+(eval-after-load "anything-config"
+  '(progn
+     (custom-set-variables '(anything-command-map-prefix-key "C-z"))
+     (setq anything-input-idle-delay 0.1)
+     ;; anything-grep
+     ;; (auto-install-from-emacswiki "anything-grep.el")
+     (require 'anything-grep)
+     ;; replace commands
+     (define-key global-map (kbd "C-x b") 'anything-for-files)
+     (define-key global-map (kbd "M-y")   'anything-show-kill-ring)
+     (define-key global-map (kbd "M-x")   'anything-M-x)
+     ;; anything comamnd
+     (define-key anything-command-map (kbd "r") 'anything-resume)
+     (define-key anything-command-map (kbd "o") 'anything-occur)
+     (define-key anything-command-map (kbd "g") 'anything-grep)
 
-;; anything-grep
-;; (auto-install-from-emacswiki "anything-grep.el")
-(autoload 'anything-grep "anything-grep" nil t)
-(custom-set-variables
- '(grep-command "ack --nogroup "))
-
-;; anything comamnd
-(define-key anything-command-map (kbd "r") 'anything-resume)
-(define-key anything-command-map (kbd "o") 'anything-occur)
-(define-key anything-command-map (kbd "g") 'anything-grep)
-
-;; my anything
-(defun my-anything ()
-  (interactive)
-  (anything-other-buffer
-   '(anything-c-source-buffers-list
-     anything-c-source-recentf)
-   "*anything my-anything*"))
-(define-key global-map (kbd "C-;") 'my-anything)
+     ;; my anything
+     (defun my-anything ()
+       (interactive)
+       (anything-other-buffer
+        '(anything-c-source-buffers-list
+          anything-c-source-recentf)
+        "*anything my-anything*"))
+     (define-key global-map (kbd "C-;") 'my-anything)))
