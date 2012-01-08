b98df406eaf20fbd4625bd1697842c94faabae12
sugyan <sugi1982@gmail.com>
2012-01-08 17:49:05 +0900

    emacs: js2-mode settings(js2-prev-error)

--- .emacs.d/inits/22-js.el
+++ .emacs.d/inits/22-js.el
@@ -6,8 +6,16 @@
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 (add-to-list 'auto-mode-alist '("\\.json$" . js-mode))
 
-;; key config
-(define-key js2-mode-map (kbd "C-m") 'newline-and-indent)
+(eval-after-load "js2-mode"
+  '(progn
+     ;; my command
+     (defun js2-prev-error ()
+       (interactive)
+       (js2-next-error -1))
+     ;; key config
+     (define-key js2-mode-map (kbd "C-m") 'newline-and-indent)
+     (define-key js2-mode-map (kbd "M-n") 'js2-next-error)
+     (define-key js2-mode-map (kbd "M-p") 'js2-prev-error)))
 
 ;; hook
 (defun my-js2-mode-hook ()
