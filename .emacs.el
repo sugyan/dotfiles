992b75cd0e5bccd21b005b64604315a85bb3bc62
sugyan <sugi1982@gmail.com>
2012-01-09 23:23:24 +0900

    emacs: rewrite init loader

--- .emacs.el
+++ .emacs.el
@@ -5,27 +5,25 @@
 ;; (auto-install-from-emacswiki "auto-install.el")
 (add-to-list 'load-path "~/.emacs.d/auto-install")
 
-;; my init loader
+;; init loader
+;; http://coderepos.org/share/browser/lang/elisp/init-loader/init-loader.el
 (eval-when-compile (require 'cl))
 (defvar init-loader-directory (expand-file-name "~/.emacs.d/inits"))
 (defvar init-loader-regexp "\\(?:^[[:digit:]]\\{2\\}-.*\.elc?\\)$")
+(defun init-loader-files (dir regexp)
+  (loop for file in (directory-files dir t)
+        when (and (string-match regexp (file-name-nondirectory file))
+                  (or (not (locate-library (concat file "c")))
+                      (string-match "\.elc$" file)))
+        collect file into ret
+        finally return (sort ret 'string<)))
 (defun init-loader-load (dir regexp)
-  (let* ((load-path (cons dir load-path))
-         (targets (loop for file in (directory-files dir t)
-                        when (and (string-match regexp (file-name-nondirectory file))
-                                  (or (not (locate-library (concat file "c")))
-                                      (string-match "\.elc$" file)))
-                        collect file into ret
-                        finally return (sort ret 'string<))))
+  (let ((load-path (cons dir load-path))
+        (targets (init-loader-files dir regexp)))
     (dolist (target targets)
       (condition-case e
-          (let* ((before (current-time))
-                 (result (load (file-name-sans-extension target) nil t))
-                 (after  (current-time))
-                 (time (+ (* (- (nth 1 after) (nth 1 before)) 1000)
-                          (/ (- (nth 2 after) (nth 2 before)) 1000))))
-            (message "loaded %s: %d msec" target time))
-        (error (message "%s" e))))))
+          (load (file-name-sans-extension target) nil t)
+        (error (message "load error: %s - %s" target e))))))
 (init-loader-load init-loader-directory init-loader-regexp)
 
 ;; show init time
