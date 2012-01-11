dab7a6b3aa6ac8f2ee624bd5c0b54712f5015117
sugyan <sugi1982@gmail.com>
2012-01-12 00:10:04 +0900

    emacs: add snippet settings

--- .emacs.d/inits/04-yasnippet.el
+++ .emacs.d/inits/04-yasnippet.el
@@ -6,5 +6,24 @@
  '(yas/snippet-dirs '("~/.emacs.d/etc/snippets")))
 (yas/global-mode t)
 
+;; anything interface
+(eval-after-load "anything-config"
+  '(progn
+     (defun my-yas/prompt (prompt choices &optional display-fn)
+       (let* ((names (loop for choice in choices
+                           collect (or (and display-fn (funcall display-fn choice))
+                                       coice)))
+              (selected (anything-other-buffer
+                         `(((name . ,(format "%s" prompt))
+                            (candidates . names)
+                            (action . (("Insert snippet" . (lambda (arg) arg))))))
+                         "*anything yas/prompt*")))
+         (if selected
+             (let ((n (position selected names :test 'equal)))
+               (nth n choices))
+           (signal 'quit "user quit!"))))
+     (custom-set-variables '(yas/prompt-functions '(my-yas/prompt)))
+     (define-key anything-command-map (kbd "y") 'yas/insert-snippet)))
+
 ;; snippet-mode for *.yasnippet
 (add-to-list 'auto-mode-alist '("\\.yasnippet$" . snippet-mode))
