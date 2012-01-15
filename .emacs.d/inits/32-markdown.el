ddff4ffd96c91661d2b1ac354de81162db9accf4
sugyan <sugi1982@gmail.com>
2012-01-15 20:08:16 +0900

    emacs: markdown-mode settings

--- .emacs.d/inits/32-markdown.el
+++ .emacs.d/inits/32-markdown.el
@@ -2,5 +2,10 @@
 
 ;; markdown-mode
 ;; (auto-install-from-url "http://jblevins.org/projects/markdown-mode/markdown-mode.el")
+;; to install markdown command (Markdown.pl) :
+;; $ cpanm Text::Markdown
 (autoload 'markdown-mode "markdown-mode" nil t)
+(custom-set-variables
+ '(markdown-command                "Markdown.pl")
+ '(markdown-command-needs-filename t))
 (add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
