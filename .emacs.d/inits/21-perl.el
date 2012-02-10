d4191def769b7576c11766d67808d2da8b1e7ff2
sugyan <sugi1982@gmail.com>
2012-02-10 18:27:44 +0900

    emacs: perl editing

--- .emacs.d/inits/21-perl.el
+++ .emacs.d/inits/21-perl.el
@@ -1,25 +1,31 @@
 ;;; Perl
 
 ;; cperl-mode
-;; (require 'cperl-mode)
-(autoload 'cperl-mode "cperl-mode" nil t)
+(defalias 'perl-mode 'cperl-mode)
+(add-to-list 'auto-mode-alist '("\\.psgi$" . cperl-mode))
 (eval-after-load "cperl-mode"
   '(progn
-     ;; indentation
-     (setq cperl-indent-level 4)
-     ;; key config
-     (define-key cperl-mode-map (kbd ";") nil)))
-(defalias 'perl-mode 'cperl-mode)
+     (define-key cperl-mode-map (kbd "C-m") 'newline-and-indent)
+     (define-key cperl-mode-map (kbd "(") nil)
+     (define-key cperl-mode-map (kbd "{") nil)))
+(custom-set-variables
+ '(cperl-indent-level           4)
+ '(cperl-indent-parens-as-block t)
+ '(cperl-close-paren-offset     -4))
 
 ;; perl-completion
 ;; (auto-install-from-emacswiki "perl-completion.el")
 (autoload 'perl-completion-mode "perl-completion" nil t)
-(defvar ac-source-my-perl-completion
-  '((candidates . plcmp-ac-make-cands)))
+(eval-after-load "perl-completion"
+  '(progn
+     (define-key plcmp-mode-map (kbd "M-TAB") nil)
+     (define-key plcmp-mode-map (kbd "M-C-o") 'plcmp-cmd-smart-complete)))
 
 ;; hook
 (defun my-cperl-mode-hook ()
   (perl-completion-mode t)
   (when (boundp 'auto-complete-mode)
+    (defvar ac-source-my-perl-completion
+      '((candidates . plcmp-ac-make-cands)))
     (add-to-list 'ac-sources 'ac-source-my-perl-completion)))
 (add-hook 'cperl-mode-hook 'my-cperl-mode-hook)
