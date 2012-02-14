1583b73e69ad5be9dc64cfd61be5243568df990c
sugyan <sugi1982@gmail.com>
2012-02-14 11:51:55 +0900

    emacs: smartchr arrow

--- .emacs.d/inits/03-editing.el
+++ .emacs.d/inits/03-editing.el
@@ -38,6 +38,7 @@
 (define-key global-map (kbd "{")  (smartchr '("{`!!'}" "{")))
 (define-key global-map (kbd "[")  (smartchr '("[`!!']" "[")))
 (define-key global-map (kbd "<")  (smartchr '("<`!!'>" "<")))
+(define-key global-map (kbd ">")  (smartchr '(">" " => ")))
 (define-key global-map (kbd "'")  (smartchr '("'`!!''" "'")))
 (define-key global-map (kbd "\"") (smartchr '("\"`!!'\"" "\"")))
 
