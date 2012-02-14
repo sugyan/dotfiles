864103874703453929da6e87b189762abeacf287
sugyan <sugi1982@gmail.com>
2012-02-14 11:52:21 +0900

    emacs: magit color settings

---
+++ .emacs.d/inits/41-magit.el
@@ -0,0 +1,9 @@
+;;; Magit
+
+;; color
+(eval-after-load 'magit
+  '(progn
+     (set-face-background 'magit-item-highlight "#202020")
+     (set-face-foreground 'magit-diff-add "#40ff40")
+     (set-face-foreground 'magit-diff-del "#ff4040")
+     (set-face-foreground 'magit-diff-file-header "#4040ff")))
