5eee201330f453e9114fcf42a1fff3555c8c795a
sugyan <sugi1982@gmail.com>
2012-02-23 18:53:22 +0900

    emacs for terminal

--- .emacs.d/inits/window/ns/00-font.el
+++ .emacs.d/inits/window/ns/00-font.el
@@ -1,5 +1,4 @@
 ;;; font
-
 ;; http://www.dafont.com/bitstream-vera-mono.font
 ;; http://www-old.gnome.org/fonts/
 (create-fontset-from-ascii-font "-apple-Bitstream_Vera_Sans_Mono-medium-normal-normal-*-14-*-*-*-m-0-iso10646-1" nil "BitstreamMarugo")
--- .emacs.el
+++ .emacs.el
@@ -18,19 +18,19 @@
 (defvar init-loader-directory (expand-file-name "~/.emacs.d/inits"))
 (defvar init-loader-regexp "\\(?:^[[:digit:]]\\{2\\}-.*\.elc?\\)$")
 (defun init-loader-files (dir regexp)
-  (loop for file in (directory-files dir t)
-        when (and (string-match regexp (file-name-nondirectory file))
-                  (or (not (locate-library (concat file "c")))
-                      (string-match "\.elc$" file)))
-        collect file into ret
-        finally return (sort ret 'string<)))
+  (let ((common-files (directory-files dir t))
+        (window-files (directory-files (mapconcat 'identity (list init-loader-directory "window" (symbol-name window-system)) "/") t)))
+    (loop for file in (append common-files window-files)
+          when (and (string-match regexp (file-name-nondirectory file))
+                    (or (not (locate-library (concat file "c")))
+                        (string-match "\.elc$" file)))
+          collect file into ret
+          finally return (sort ret 'string<))))
 (defun init-loader-load (dir regexp)
-  (let ((load-path (cons dir load-path))
-        (targets (init-loader-files dir regexp)))
-    (dolist (target targets)
-      (condition-case e
-          (load (file-name-sans-extension target) nil t)
-        (error (message "load error: %s - %s" target e))))))
+  (dolist (target (init-loader-files dir regexp))
+    (condition-case e
+        (load (file-name-sans-extension target) nil t)
+      (error (message "load error: %s - %s" target e)))))
 (init-loader-load init-loader-directory init-loader-regexp)
 
 ;; show init time
