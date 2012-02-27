cef179a58fcfdb649d3b6fdc470578bbf6ccf6a1
sugyan <sugi1982@gmail.com>
2012-02-27 17:43:07 +0900

    .emacs.d/inits/32-yaml.el: modify yaml settings

--- .emacs.d/inits/32-yaml.el
+++ .emacs.d/inits/32-yaml.el
@@ -3,4 +3,4 @@
 ;; yaml-mode
 ;; (auto-install-from-url "https://raw.github.com/yoshiki/yaml-mode/master/yaml-mode.el")
 (autoload 'yaml-mode "yaml-mode" nil t)
-(add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
+(add-to-list 'auto-mode-alist '("\\.ya?ml$" . yaml-mode))
