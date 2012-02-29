ee53449e81b1871196ae5c02733e419b6e9127cd
sugyan <sugi1982@gmail.com>
2012-02-29 14:17:55 +0900

    modify create_symlink.sh

--- create_symlink.sh
+++ create_symlink.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 cd $(dirname $0)
-for dotfile in .?*
-do
-    if [ $dotfile != '..' ] && [ $dotfile != '.git' ]
-    then
-        ln -Fis "$PWD/$dotfile" $HOME
-    fi
+for dotfile in .?*; do
+    case $dotfile in
+        *.elc)
+            continue;;
+        ..)
+            continue;;
+        .git)
+            continue;;
+        *)
+            ln -Fis "$PWD/$dotfile" $HOME
+            ;;
+    esac
 done
