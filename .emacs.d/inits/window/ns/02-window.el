2dfadf2e62ac2dc2ef1289d361f2a5c9006658e2
sugyan <sugi1982@gmail.com>
2012-03-01 19:44:58 +0900

    emacs: modify transparency

--- .emacs.d/inits/window/ns/02-window.el
+++ .emacs.d/inits/window/ns/02-window.el
@@ -3,5 +3,5 @@
  '(initial-frame-alist
    '((width  . 177)
      (height . 49)
-     (alpha  . 85)))
- '(scroll-bar-mode     nil))                           ; hide scroll bar
+     (alpha  . 80)))
+ '(scroll-bar-mode nil))            ; hide scroll bar
