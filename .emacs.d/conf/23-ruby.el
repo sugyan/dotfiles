021a7032dbe2e4d442e7ea2b2cd424a9646ad50b
sugyan <sugi1982@gmail.com>
2012-05-01 23:20:23 +0900

    emacs: ruby settings

--- .emacs.d/conf/23-ruby.el
+++ .emacs.d/conf/23-ruby.el
@@ -1,10 +1,13 @@
 ;;; Ruby
 
+;; ruby-mode
+;; https://raw.github.com/ruby/ruby/v1_9_3_194/misc/ruby-mode.el
+
 (custom-set-variables
  '(ruby-deep-indent-paren nil)) ;; indent
 
 ;; ruby-electric.el
-;; (auto-install-from-url "https://raw.github.com/ruby/ruby/trunk/misc/ruby-electric.el")
+;; (auto-install-from-url "https://raw.github.com/ruby/ruby/v1_9_3_194/misc/ruby-electric.el")
 (autoload 'ruby-electric-mode "ruby-electric" nil t)
 
 ;; hook
