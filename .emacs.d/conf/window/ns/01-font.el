14e651e5eb838e6f2668f961826dfcdb2033e48b
sugyan <sugi1982@gmail.com>
2012-08-28 00:54:23 +0900

    emacs: add comment for font setting

--- .emacs.d/conf/window/ns/01-font.el
+++ .emacs.d/conf/window/ns/01-font.el
@@ -2,6 +2,7 @@
 ;; http://www.dafont.com/bitstream-vera-mono.font
 ;; http://www-old.gnome.org/fonts/
 ;; http://yumisuke.net/297/emacs/
+;; http://sourceforge.jp/projects/macemacsjp/lists/archive/users/2011-January/001686.html
 (create-fontset-from-ascii-font "-apple-Bitstream_Vera_Sans_Mono-medium-normal-normal-*-14-*-*-*-m-0-iso10646-1" nil "BitstreamMarugo")
 (set-frame-font "fontset-bitstreammarugo")
 (set-fontset-font (frame-parameter nil 'font)
