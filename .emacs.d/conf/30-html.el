8473e3aa7b14764db5d373a8a5dea3b06c781a06
sugyan <sugi1982@gmail.com>
2013-03-28 17:00:29 +0900

    emacs: update html settings

--- .emacs.d/conf/30-html.el
+++ .emacs.d/conf/30-html.el
@@ -5,6 +5,8 @@
   (when (boundp 'auto-complete-mode)
     (eval
      '(progn
+        (when (functionp 'smartchr)
+          (define-key html-mode-map (kbd "%") (smartchr '("%" "[% `!!' %]"))))
         (add-to-list 'ac-modes 'html-mode)
         (add-to-list 'ac-sources 'ac-source-dictionary)))))
 (add-hook 'html-mode-hook 'my-html-mode-hook)
