9cec385a58612ef2298a1b3fd6ce84d7f51500ed
sugyan <sugi1982@gmail.com>
2014-02-02 23:50:20 +0900

    emacs: add initial PATH for zsh

--- .emacs.d/conf/window/ns/00-env.el
+++ .emacs.d/conf/window/ns/00-env.el
@@ -1,8 +1,8 @@
 ;;; environment variables
 (eval-when-compile (require 'cl))
 ;; set environment variable `PATH' from zsh configuration
-(setenv "PATH"
-        (substring (shell-command-to-string "~/local/bin/zsh -ic 'echo $PATH' 2> /dev/null") 0 -1))
+(setenv "PATH" (mapconcat 'identity (list "~/local/bin" "/usr/local/bin" (getenv "PATH")) ":"))
+(setenv "PATH" (substring (shell-command-to-string "$(which zsh) -ic 'echo $PATH' 2> /dev/null") 0 -1))
 ;; add PATH to 'exec-path
 (loop for path in (reverse (split-string (getenv "PATH") ":"))
       do (add-to-list 'exec-path path))
