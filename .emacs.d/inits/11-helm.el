5fac8d95e5da49dcb9e9cb9b74502fb0edf1a678
sugyan <sugi1982@gmail.com>
2014-02-06 22:26:38 +0900

    emacs: update helm config. use helm-config!

--- .emacs.d/inits/11-helm.el
+++ .emacs.d/inits/11-helm.el
@@ -1,29 +1,32 @@
 ;;; helm
 ;; (package-install 'helm)
-
 (require 'helm)
-(require 'helm-files)
+
 ;; customize
 (custom-set-variables
  '(helm-truncate-lines t)
  '(helm-delete-minibuffer-contents-from-point t)
  '(helm-ff-skip-boring-files t)
- '(helm-boring-file-regexp-list '("\\.hg$" "\\.svn$" "\\.CVS$" "\\._darcs$" "\\.la$" "\\.o$" "~$" "\\.elc$")))
+ '(helm-boring-file-regexp-list '("~$" "\\.elc$")))
+
+;; set helm-command-prefix-key to "C-z"
+(progn
+  (require 'helm-config)
+  (global-unset-key (kbd "C-z"))
+  (custom-set-variables
+   '(helm-command-prefix-key "C-z")))
 
 ;; key settings
-(define-key helm-map (kbd "C-h") 'delete-backward-char)
-(define-key helm-find-files-map (kbd "C-h") 'helm-ff-backspace)
-(define-key helm-find-files-map (kbd "C-i") 'helm-execute-persistent-action)
-
 (global-set-key (kbd "C-q") 'helm-mini)
 (global-set-key (kbd "M-x") 'helm-M-x)
 (global-set-key (kbd "M-y") 'helm-show-kill-ring)
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
 (global-set-key (kbd "C-x C-b") 'helm-buffers-list)
+(define-key helm-command-map (kbd "g") 'helm-ag)
+(define-key helm-command-map (kbd "o") 'helm-occur)
 
-(global-unset-key (kbd "C-z"))
-(global-set-key (kbd "C-z a") 'helm-apropos)
-(global-set-key (kbd "C-z g") 'helm-ag)
-(global-set-key (kbd "C-z i") 'helm-imenu)
-(global-set-key (kbd "C-z o") 'helm-occur)
-(global-set-key (kbd "C-z r") 'helm-resume)
+(define-key helm-map (kbd "C-h") 'delete-backward-char)
+(eval-after-load "helm-files"
+  '(progn
+     (define-key helm-find-files-map (kbd "C-h") 'helm-ff-backspace)
+     (define-key helm-find-files-map (kbd "C-i") 'helm-execute-persistent-action)))
