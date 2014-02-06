3abe9ac0e723621bac31e0f6b9cbc5ce2a4a40c7
sugyan <sugi1982@gmail.com>
2014-02-06 22:57:18 +0900

    emacs: update yasnippet setting

--- .emacs.d/inits/04-yasnippet.el
+++ .emacs.d/inits/04-yasnippet.el
@@ -9,24 +9,24 @@
 (define-key yas-minor-mode-map (kbd "M-o") 'yas-expand)
 (define-key yas-minor-mode-map (kbd "TAB") nil)
 
-;; anything interface
-(eval-after-load "anything-config"
+;; helm interface
+;; http://d.hatena.ne.jp/syohex/20121207/1354885367
+(eval-after-load "helm"
   '(progn
      (defun my-yas/prompt (prompt choices &optional display-fn)
        (let* ((names (loop for choice in choices
                            collect (or (and display-fn (funcall display-fn choice))
                                        choice)))
-              (selected (anything-other-buffer
+              (selected (helm-other-buffer
                          `(((name . ,(format "%s" prompt))
                             (candidates . names)
                             (action . (("Insert snippet" . (lambda (arg) arg))))))
-                         "*anything yas/prompt*")))
+                         "*helm yas/prompt*")))
          (if selected
              (let ((n (position selected names :test 'equal)))
                (nth n choices))
            (signal 'quit "user quit!"))))
-     (custom-set-variables '(yas/prompt-functions '(my-yas/prompt)))
-     (define-key anything-command-map (kbd "y") 'yas/insert-snippet)))
+     (custom-set-variables '(yas/prompt-functions '(my-yas/prompt)))))
 
 ;; snippet-mode for *.yasnippet files
 (add-to-list 'auto-mode-alist '("\\.yasnippet$" . snippet-mode))
