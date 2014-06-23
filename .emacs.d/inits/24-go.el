4aa57db14bc680534157127c99670698b807ab95
sugyan <sugi1982@gmail.com>
2014-06-23 20:41:43 +0900

    emacs: add go settings

--- .emacs.d/inits/24-go.el
+++ .emacs.d/inits/24-go.el
@@ -1,7 +1,18 @@
 ;;; Go
+;; (package-install 'go-mode)
+;; (package-install 'helm-go-package)
 
 (add-hook 'before-save-hook #'gofmt-before-save)
 
 (eval-after-load "go-mode"
   '(progn
-     (define-key go-mode-map (kbd "C-c g") 'helm-go-package)))
+     (define-key go-mode-map (kbd "C-c d") 'helm-go-package)))
+
+;; use "Show documentation" as 1st action
+(eval-after-load "helm-go-package"
+  '(progn
+     (let* ((actions (helm-attr 'action helm-source-go-package))
+            (godoc (nth 2 actions)))
+       (delete godoc actions)
+       (push godoc actions)
+       (helm-attrset 'action actions helm-source-go-package))))
