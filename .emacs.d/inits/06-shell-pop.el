debe72d15cfe62332f99d71097b58acb4c6f9ee8
sugyan <sugi1982@gmail.com>
2014-06-24 19:39:34 +0900

    emacs: shell-pop setting

--- .emacs.d/inits/06-shell-pop.el
+++ .emacs.d/inits/06-shell-pop.el
@@ -5,3 +5,8 @@
  '(shell-pop-universal-key (kbd "C-t"))
  '(shell-pop-shell-type '("ansi-term" "*ansi-term*" (lambda () (ansi-term shell-pop-term-shell))))
  '(shell-pop-term-shell (substring (shell-command-to-string "which zsh") 0 -1)))
+
+;; ansi-term keybinding
+(require 'term)
+(define-key term-raw-map (kbd "C-q") nil)
+(define-key term-raw-map (kbd "C-o") nil)
