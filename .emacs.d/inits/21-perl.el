92903b9a0e82ee2d3272f4521c096c122e06ee90
sugyan <sugi1982@gmail.com>
2014-06-26 17:07:59 +0900

    emacs: no flymake, use flycheck only!

--- .emacs.d/inits/21-perl.el
+++ .emacs.d/inits/21-perl.el
@@ -1,13 +1,22 @@
-;;; Perl --- perl
+;;; 21-perl.el --- perl setting
 
 ;;; Commentary:
+;; `cperl-mode':
 ;; cperl-mode from package-install for cperl-indent-subs-specially
 ;; http://d.hatena.ne.jp/syohex/20120818/1345302707
 ;; (package-install 'cperl-mode)
+;; `helm-perldoc':
+;; (package-install 'helm-perldoc)
 
 ;;; Code:
 
 (defalias 'perl-mode 'cperl-mode)
+
+(custom-set-variables
+ '(cperl-indent-parens-as-block t)
+ '(cperl-close-paren-offset     -4)
+ '(cperl-indent-subs-specially  nil))
+
 (add-to-list 'auto-mode-alist '("\\.psgi$" . cperl-mode))
 (add-to-list 'auto-mode-alist '("\\.t\\'"  . cperl-mode))
 (add-to-list 'auto-mode-alist '("cpanfile" . cperl-mode))
@@ -15,36 +24,44 @@
   '(progn
      (helm-perldoc:setup)
      (cperl-set-style "PerlStyle")
+     ;; my flycheck checkers
+     (flycheck-define-checker my-perl
+       "A Perl syntax checker using the Perl interpreter."
+       :command ("perl" "-w" "-c"
+                 (eval (let ((options '()))
+                         (when (projectile-project-p)
+                           (push (concat "-I" (projectile-project-root)) options)
+                           (push (concat "-I" (projectile-expand-root "lib")) options)
+                           (when (projectile-verify-file "cpanfile")
+                             (push (concat "-I" (projectile-expand-root "local/lib/perl5")) options))
+                           options)))
+                 source)
+       :error-patterns ((error line-start (minimal-match (message)) " at " (file-name) " line " line (or "." (and ", " (zero-or-more not-newline))) line-end))
+       :modes (perl-mode cperl-mode)
+       :next-checkers (my-perl-perlcritic))
+     (flycheck-define-checker my-perl-perlcritic
+       "A Perl syntax checker using Perl::Critic."
+       :command ("perlcritic" "--no-color" "--verbose" "%f:%l:%c:%s:%m (%e)\n"
+                 (option "--severity" flycheck-perlcritic-verbosity flycheck-option-int)
+                 source-original)
+       :error-patterns ((info    line-start (file-name) ":" line ":" column ":" (any "1")   ":" (message) line-end)
+                        (warning line-start (file-name) ":" line ":" column ":" (any "234") ":" (message) line-end)
+                        (error   line-start (file-name) ":" line ":" column ":" (any "5")   ":" (message) line-end))
+       :modes (cperl-mode perl-mode))
+     (add-to-list 'flycheck-checkers 'my-perl)
+     (add-to-list 'flycheck-checkers 'my-perl-perlcritic)
+     ;; key bind
      (define-key cperl-mode-map (kbd "C-m") 'newline-and-indent)
-     (define-key cperl-mode-map (kbd "M-n") 'flymake-goto-next-error)
-     (define-key cperl-mode-map (kbd "M-p") 'flymake-goto-prev-error)
      (define-key cperl-mode-map (kbd "C-c d") 'helm-perldoc)
      (define-key cperl-mode-map (kbd "C-c h") 'helm-perldoc:history)))
-(custom-set-variables
- '(cperl-indent-parens-as-block t)
- '(cperl-close-paren-offset     -4)
- '(cperl-indent-subs-specially  nil))
-
-;; flymake
-(defadvice flymake-perl-init (after flymake-perl-init-add-libs activate)
-  "Modify perl-init function."
-  (require 'projectile)
-  (when (projectile-project-p)
-    (when (projectile-verify-file "cpanfile")
-      (push (concat "-I" (projectile-expand-root "local/lib/perl5")) (nth 1 ad-return-value)))
-    (push (concat "-I" (projectile-expand-root "lib")) (nth 1 ad-return-value))
-    (push (concat "-I" (projectile-project-root)) (nth 1 ad-return-value))))
 
 ;; hook
-(require 'helm-perldoc)
+(autoload 'helm-perldoc:carton-setup "helm-perldoc" nil t)
+(autoload 'flycheck-select-checker "flycheck" nil t)
 (defun my-cperl-mode-hook ()
   "Hook function for `cperl-mode'."
   (helm-perldoc:carton-setup)
-  (flymake-mode t)
-  (when (boundp 'auto-complete-mode)
-    (eval
-     '(progn
-        (add-to-list 'ac-sources 'ac-source-dictionary)))))
+  (flycheck-select-checker 'my-perl))
 (add-hook 'cperl-mode-hook 'my-cperl-mode-hook)
 
 ;;; 21-perl.el ends here
