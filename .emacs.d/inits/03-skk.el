970c9c1aaf1c8211c5b90df978e41259341042d8
sugyan <sugi1982@gmail.com>
2014-10-20 14:41:17 +0900

    emacs: modify 03-skk.el

--- .emacs.d/inits/03-skk.el
+++ .emacs.d/inits/03-skk.el
@@ -1,10 +1,15 @@
-;;; SKK
+;;; 03-skk.el --- skk settings
+
+;;; Commentary:
 ;; $ curl -O http://openlab.ring.gr.jp/skk/maintrunk/ddskk-15.1.tar.gz
 ;; $ tar zxvf ddskk-15.1.tar.gz
 ;; $ cd ddskk-15.1
 ;; $ make install EMACS=/Applications/Emacs.app/Contents/MacOS/Emacs
 
+;;; Code:
 (require 'skk-autoloads)
 (custom-set-variables
  '(skk-sticky-key ";"))
 (define-key global-map (kbd "C-x C-m") 'skk-mode)
+
+;;; 03-skk.el ends here
