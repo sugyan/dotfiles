ce8f441b67c6d5f95d9010de54aeefaa9f9c4b4b
sugyan <sugi1982@gmail.com>
2015-03-11 15:09:53 +0900

    emacs: update font setting for cocoa

--- .emacs.d/inits/cocoa-emacs-01-font.el
+++ .emacs.d/inits/cocoa-emacs-01-font.el
@@ -1,13 +1,18 @@
-;;; font
+;;; cocoa-emacs-01-font.el --- Font setting
+
+;;; Commentary:
 ;; http://www.dafont.com/bitstream-vera-mono.font
-;; http://www-old.gnome.org/fonts/
 ;; http://yumisuke.net/297/emacs/
 ;; http://sourceforge.jp/projects/macemacsjp/lists/archive/users/2011-January/001686.html
-(create-fontset-from-ascii-font "-apple-Bitstream_Vera_Sans_Mono-medium-normal-normal-*-14-*-*-*-m-0-iso10646-1" nil "BitstreamMarugo")
-(set-frame-font "fontset-bitstreammarugo")
+;; http://emacs.g.hatena.ne.jp/sakito/20100127
+
+;;; Code:
+(create-fontset-from-ascii-font "-*-Bitstream Vera Sans Mono-normal-normal-normal-*-12-*-*-*-m-0-iso10646-1" nil "BitstreamMarugo")
 (set-fontset-font (frame-parameter nil 'font)
                   'unicode
                   (font-spec :family "Hiragino Maru Gothic ProN")
                   nil
                   'append)
 (setq face-font-rescale-alist '(("Hiragino.*" . 1.2)))
+
+;;; cocoa-emacs-01-font.el ends here
