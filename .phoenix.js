053eed4b0074bdeddb88242dfdb20c218e2c6ccc
sugyan <sugi1982@gmail.com>
2016-05-08 23:02:35 +0900

    update .phoenix.js

--- .phoenix.js
+++ .phoenix.js
@@ -1,8 +1,7 @@
 var keys = [];
 
 var hotkey = ['ctrl', 'cmd'];
-keys.push(Phoenix.bind('q', hotkey, function() { App.launch('iTerm').focus();         }));
-keys.push(Phoenix.bind('w', hotkey, function() { App.launch('Emacs').focus();         }));
-keys.push(Phoenix.bind('e', hotkey, function() { App.launch('Google Chrome').focus(); }));
-keys.push(Phoenix.bind('t', hotkey, function() { App.launch('Twitter').focus();       }));
-Phoenix.log('OK!!');
+keys.push(Phoenix.bind('q', hotkey, function() { App.launch('iTerm').focus();     }));
+keys.push(Phoenix.bind('w', hotkey, function() { App.launch('Emacs').focus();     }));
+keys.push(Phoenix.bind('e', hotkey, function() { App.launch('Vivaldi').focus();   }));
+keys.push(Phoenix.bind('t', hotkey, function() { App.launch('Night Owl').focus(); }));
