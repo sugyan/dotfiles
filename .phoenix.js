7e0316f35e0222f536f3fb24398b56635d567a90
sugyan <sugi1982@gmail.com>
2016-05-09 22:42:41 +0900

    goodbye vivaldi

--- .phoenix.js
+++ .phoenix.js
@@ -1,7 +1,7 @@
 var keys = [];
 
 var hotkey = ['ctrl', 'cmd'];
-keys.push(Phoenix.bind('q', hotkey, function() { App.launch('iTerm').focus();     }));
-keys.push(Phoenix.bind('w', hotkey, function() { App.launch('Emacs').focus();     }));
-keys.push(Phoenix.bind('e', hotkey, function() { App.launch('Vivaldi').focus();   }));
-keys.push(Phoenix.bind('t', hotkey, function() { App.launch('Night Owl').focus(); }));
+keys.push(Phoenix.bind('q', hotkey, function() { App.launch('iTerm').focus();         }));
+keys.push(Phoenix.bind('w', hotkey, function() { App.launch('Emacs').focus();         }));
+keys.push(Phoenix.bind('e', hotkey, function() { App.launch('Google Chrome').focus(); }));
+keys.push(Phoenix.bind('t', hotkey, function() { App.launch('Night Owl').focus();     }));
