1c1249860384f9e91baf5007ed29eb40a43ad7b7
sugyan <sugi1982@gmail.com>
2022-10-25 15:03:07 +0900

    Update alacritty settings, and hammerspoon key bindings

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -23,7 +23,6 @@ launcher({'cmd', 'ctrl'}, 'w', 'Visual Studio Code')
 launcher({'cmd', 'ctrl'}, 'e', 'Google Chrome')
 launcher({'cmd', 'ctrl'}, 's', 'Slack')
 launcher({'cmd', 'ctrl'}, 'l', 'LINE')
-launcher({'cmd', 'ctrl'}, 'x', 'Xcode')
 launcher({'cmd', 'ctrl'}, 't', 'Twitter')
 launcher({'cmd', 'ctrl'}, 'd', 'Discord')
 launcher({'cmd', 'ctrl'}, 'o', 'Obsidian')
@@ -39,61 +38,10 @@ local function remap(mods, key, fn)
   return hs.hotkey.bind(mods, key, fn, nil, fn)
 end
 
--- global
 remap({'ctrl'}, 'h', keyStroke({}, 'delete'))
 remap({'ctrl'}, '[', keyStroke({}, 'escape'))
 remap({'ctrl'}, 'j', keyStroke({}, 'return'))
 remap({'ctrl', 'cmd'}, 'j', keyStroke({'cmd'}, 'return'))
-
--- disable when a specific application is active
-local remapKeys = {
-  remap({'ctrl'}, 'b', keyStroke({}, 'left')),
-  remap({'ctrl'}, 'f', keyStroke({}, 'right')),
-  remap({'ctrl'}, 'n', keyStroke({}, 'down')),
-  remap({'ctrl'}, 'p', keyStroke({}, 'up')),
-  remap({'ctrl'}, 'y', keyStroke({'cmd'}, 'v'))
-}
-
-local function handleGlobalAppEvent(name, event, app)
-  if event == hs.application.watcher.activated then
-    if name == 'iTerm2' or name == 'Code' then
-      for i, key in ipairs(remapKeys) do
-        key:disable()
-      end
-    else
-      for i, key in ipairs(remapKeys) do
-        key:enable()
-      end
-    end
-  end
-end
-
-appsWatcher = hs.application.watcher.new(handleGlobalAppEvent)
-appsWatcher:start()
-
-
--- SKK
--- local stickyShift = false
--- local targets = {}
--- for i = 96, 122 do
---   targets[hs.keycodes.map[string.char(i)]] = true
--- end
-
--- hs.hotkey.bind({}, ';', function()
---   method = hs.keycodes.currentMethod()
---   if method and method:find('AquaSKK') then
---     stickyShift = true
---   else
---     hs.eventtap.keyStrokes(';')
---   end
--- end)
-
--- keyTap = hs.eventtap.new({hs.eventtap.event.types.keyDown}, function(event)
---   if stickyShift and targets[event:getKeyCode()] then
---     event:setFlags({shift = true})
---   end
---   stickyShift = false
--- end):start()
 
 hs.hotkey.bind({'ctrl'}, 'm', function()
   if hs.keycodes.currentMethod():find('AquaSKK') then
